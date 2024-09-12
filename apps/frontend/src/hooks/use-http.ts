1c603ec8c15ba8d05a8b34cbfbf1c4f0b55045d7
evansmj <evansmj500@gmail.com>
2024-09-11 22:31:32 -0400

    Add VolumeGraph

--- apps/frontend/src/hooks/use-http.ts
+++ apps/frontend/src/hooks/use-http.ts
@@ -6,8 +6,8 @@ import { AppContext } from '../store/AppContext';
 import { ApplicationConfiguration } from '../types/app-config.type';
 import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
 import { isCompatibleVersion } from '../utilities/data-formatters';
-import { BalanceSheetSQL, SatsFlowSQL } from '../sql/bookkeeper-sql';
-import { transformToBalanceSheet, transformToSatsFlow } from '../sql/bookkeeper-transform';
+import { BalanceSheetSQL, SatsFlowSQL, VolumeSQL } from '../sql/bookkeeper-sql';
+import { transformToBalanceSheet, transformToSatsFlow, transformToVolumeData } from '../sql/bookkeeper-transform';
 
 let intervalID;
 let localAuthStatus: any = null;
@@ -174,6 +174,11 @@ const useHttp = () => {
       .then((response) => transformToSatsFlow(response.data, timeGranularity, hideZeroActivityPeriods));
   };
 
+  const getVolumeData = () => {
+    return sendRequest(false, 'post', 'cln/call', { 'method': 'sql', 'params': [VolumeSQL] })
+      .then((response) => transformToVolumeData(response.data));
+  };
+
   const clnSendPayment = (paymentType: PaymentType, invoice: string, amount: number | null) => {
     if (paymentType === PaymentType.KEYSEND) {
       return sendRequest(true, 'post', '/cln/call', { 'method': 'keysend', 'params': { 'destination': invoice, 'amount_msat': amount } });
@@ -316,6 +321,7 @@ const useHttp = () => {
     createInvoiceRune,
     getBalanceSheet,
     getSatsFlow,
+    getVolumeData,
     userLogin,
     resetUserPassword,
     userLogout
